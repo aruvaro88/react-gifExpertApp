50dbe92a26f24320110ea3def470923f5e7f3091
Alvaro Moral <aruvaro@gmail.com>
2020-08-11 11:22:27 +0200

    docs added

---
+++ docs/precache-manifest.3d9e3e232c4a687b69fbc97be5479fb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6157e15ea6ed7a732134d2d43868f899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02bb421df6dfe20bf956",
+    "url": "/static/css/main.abaf6b3f.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bb421df6dfe20bf956",
+    "url": "/static/js/main.abdc64da.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
